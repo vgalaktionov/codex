e923a2097c7cfee15639c8b027533eccfa19ac2d
Vadim Galaktionov <v.galaktionov@enpicom.com>
2022-01-06 20:26:31 +0100

    does this help

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { PUBLIC_ROUTES, SECRET_KEY } from '../lib/auth';
 
 export function middleware(req: NextRequest) {
-    if (!PUBLIC_ROUTES.includes(req.url)) {
+    if (!PUBLIC_ROUTES.includes(req.url.split('/')[1])) {
         try {
             jwt.verify(req.cookies['token'], SECRET_KEY);
         } catch (error) {
