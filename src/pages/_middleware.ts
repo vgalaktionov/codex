859b0ddbb22d4bfa08569a026b65285a22043c9b
Vadim Galaktionov <v.galaktionov@enpicom.com>
2022-01-06 21:08:22 +0100

    this should work everywhere

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,7 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { PUBLIC_ROUTES, SECRET_KEY } from '../lib/auth';
 
 export function middleware(req: NextRequest) {
-    const path = new URL(req.url).pathname;
+    let path: string;
+    try {
+        path = new URL(req.url).pathname;
+    } catch (error) {
+        path = req.url;
+    }
     if (!PUBLIC_ROUTES.includes(path) && !/(jpeg|png|ico|webmanifest)$/.test(path)) {
         try {
             jwt.verify(req.cookies['token'], SECRET_KEY);
