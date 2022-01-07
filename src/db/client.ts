91b575348a2dc06bc84e5de1006a54286f5b9490
Vadim Galaktionov <v.galaktionov@enpicom.com>
2022-01-07 12:32:12 +0100

    catch migration error and log

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,9 @@
 import postgres from 'postgres';
 import shift from 'postgres-shift';
+import { log } from '../lib/util';
 
 const sql = postgres(process.env.DATABASE_URL ?? 'postgres://vadim@localhost:5432/codex');
 
-shift({ sql });
+shift({ sql }).catch(log.error);
 
 export default sql;
